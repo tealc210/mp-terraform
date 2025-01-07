e9d6af13f6e2beb2ed6027257657dec939465f4b
David A. <agbo.dvd@gmail.com>
2025-01-07 17:39:08 +0100

    rename SG resource

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -2,7 +2,7 @@ data aws_vpc "aws_training_vpc" {
     id = var.vpc_id
 }
 
-resource "aws_security_group" "allow_web" {
+resource "aws_security_group" "sg_web" {
   name        = "SG_WEB"
   description = "Allow WEB inbound traffic and all outbound traffic"
   vpc_id      = data.aws_vpc.main.id
@@ -13,27 +13,27 @@ resource "aws_security_group" "allow_web" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.allow_web.id
+  security_group_id = aws_security_group.sg_web.id
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls" {
-  security_group_id = aws_security_group.allow_web.id
+  security_group_id = aws_security_group.sg_web.id
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http_8080" {
-  security_group_id = aws_security_group.allow_web.id
+  security_group_id = aws_security_group.sg_web.id
   from_port         = 8080
   ip_protocol       = "tcp"
   to_port           = 8080
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
-  security_group_id = aws_security_group.allow_web.id
-  ip_protocol = tcp
+  security_group_id = aws_security_group.sg_web.id
+  ip_protocol = "tcp"
 }
