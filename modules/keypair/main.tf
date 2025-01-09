109593416fafd3ca197a12541926c7e3011ae6c1
David A. <me@myself.i>
2025-01-09 16:15:39 +0100

    fix missing arg

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,11 +1,12 @@
 resource "null_resource" "ssh_key" {
   provisioner "local-exec" {
-    command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
+    command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N '${var.key_passphrase}'"
   }
 }
 
 data "local_file" "ssh_key" {
   filename = "${var.keyfile_path}/${var.keyfile_name}.pub"
+  depends_on = [ null_resource.ssh_key ]
 }
 
 resource "aws_key_pair" "deployer" {
