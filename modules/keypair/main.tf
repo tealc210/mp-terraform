6ef519fd73e7404b16639fa3245a0ac23f9ff945
David A. <agbo.dvd@gmail.com>
2025-01-09 13:04:24 +0100

    test external datasource

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -4,14 +4,18 @@
 #   }
 # }
 
+data "external" "ssh_key_generator" {
+  program = [ "ssh-keygen", "-qt", "rsa", "-b", "4096", "-f", "${var.keyfile_path}/${var.keyfile_name}", "-N", "${var.key_passphrase}", "&&", "echo", "${var.keyfile_name}.pub" ]
+}
+
 resource "aws_key_pair" "deployer" {
 
-  provisioner "local-exec" {
-    command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
-  }
+  # provisioner "local-exec" {
+  #   command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
+  # }
   
   key_name   = var.keyfile_name
-  public_key = file("${var.keyfile_path}/${var.keyfile_name}.pub")
+  public_key = data.external.ssh_key_generator.result #file("${var.keyfile_path}/${var.keyfile_name}.pub")
 
   #depends_on = [ null_resource.ssh_key ]
 }
