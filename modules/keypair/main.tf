e26bb974521421cad21fd0c8eefb2945e324075d
David A. <me@myself.i>
2025-01-09 15:36:54 +0100

    fix: pass keyfile content to resource

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,21 +1,21 @@
-# resource "null_resource" "ssh_key" {
-#   provisioner "local-exec" {
-#     command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
-#   }
-# }
+resource "null_resource" "ssh_key" {
+  provisioner "local-exec" {
+    command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
+  }
+}
 
-data "external" "ssh_key_generator" {
-  program = [ "ssh-keygen", "-qt", "rsa", "-b", "4096", "-f", "${var.keyfile_path}/${var.keyfile_name}", "-N", "${var.key_passphrase}", "&&", "echo", "${var.keyfile_name}.pub" ]
+data "local_file" "ssh_key" {
+  filename = "${var.keyfile_path}/${var.keyfile_name}.pub"
 }
 
 resource "aws_key_pair" "deployer" {
 
-  # provisioner "local-exec" {
-  #   command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
-  # }
+  #provisioner "local-exec" {
+  #  command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
+  #}
   
   key_name   = var.keyfile_name
-  public_key = data.external.ssh_key_generator.result #file("${var.keyfile_path}/${var.keyfile_name}.pub")
+  public_key = data.local_file.ssh_key.content #file("${var.keyfile_path}/${var.keyfile_name}.pub")
 
   #depends_on = [ null_resource.ssh_key ]
 }
