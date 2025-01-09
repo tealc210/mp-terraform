3cd3e50258a328d95949593dd1933d3979c96dba
David A. <me@myself.i>
2025-01-09 09:41:57 +0100

    add SSH key pair module

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,12 +1,17 @@
-resource "null_resource" "ssh_key" {
+# resource "null_resource" "ssh_key" {
+#   provisioner "local-exec" {
+#     command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
+#   }
+# }
+
+resource "aws_key_pair" "deployer" {
+
   provisioner "local-exec" {
     command = "ssh-keygen -t rsa -b 4096 -f ${var.keyfile_path}/${var.keyfile_name} -N ${var.key_passphrase}"
   }
-}
-
-resource "aws_key_pair" "deployer" {
+  
   key_name   = var.keyfile_name
   public_key = file("${var.keyfile_path}/${var.keyfile_name}.pub")
 
-  depends_on = [ null_resource.ssh_key ]
+  #depends_on = [ null_resource.ssh_key ]
 }
